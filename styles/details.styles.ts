cac71fc552ff0d696716cfe438aa4b47192da63c
alexandre <alexandre020602@gmail.com>
2026-03-26 23:27:14 -0300

    DetailsStyle.ts

---
+++ styles/details.styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: { padding: 16, gap: 12 },
+  name: { fontSize: 28, fontWeight: 'bold', textTransform: 'capitalize', textAlign: 'center' },
+  label: { fontSize: 16 },
+  section: { fontSize: 20, fontWeight: 'bold', marginTop: 8 },
+});
